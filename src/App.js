e85815fa8b962984a327fa0e0890c80d1fcf95d6
Danielle Nourok <dnourok1@gmail.com>
2019-11-03 16:47:42 -0500

    skeleton of app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// External
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+  render() {
+
+    return (
+        <div>App Test</div>
+    )
+  }
+}
---
+++ src/utils/dictionary.js
@@ -0,0 +1,11 @@
+const dictionary = {
+    FORM_NAME: "New Assistance Request",
+    FIRST_NAME: "First Name",
+    LAST_NAME: "Last Name",
+    EMAIL_ADDRESS: "Email Address",
+    SELECT_TYPE: "Select Service Type",
+    PRIVACY_POLICY: "I herby accept the terms of service for THE NETWORK and the privacy policy",
+    SUBMIT: "Get Assistance"
+}
+
+export default dictionary;
